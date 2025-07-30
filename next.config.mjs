79ad149e220a9bc3af7ca1a8b1e5ef49c84013c1
w3cdpass <killu1875@gmail.com>
2025-07-30 12:29:24 +0530

    fix: Remove babel-plugin-transform-remove-console and update configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    swcMinify: true,
+    compiler: {
+        compiler: {
+            removeConsole: process.env.NODE_ENV === 'production',
+        },
+    },
     images: {
-        domains:['res.cloudinary.com', 'cdn.pixabay.com']
-    }
+        domains: ['res.cloudinary.com', 'cdn.pixabay.com']
+    },
 };
 
 export default nextConfig;
